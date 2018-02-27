a55eda03bc5f601eaab42d11f34c35607fd72b1c
jwieb325 <jwieb325@mtroyal.ca>
2018-02-26 23:48:19 -0700

    initial commit

---
+++ Polyomino.hs
@@ -0,0 +1,71 @@
+module Polyomino
+  ( Polyomino (Polyomino, token, parts, width)
+  , createPolyomino
+  , flipxy
+  , flipv
+  ) where
+
+import Types
+import Data.List
+data Polyomino = Polyomino { parts::[Location]
+                           , width::Int
+                           , height::Int
+                           , token::Token
+                           }
+                 deriving Eq
+
+createPolyomino :: [Location] -> Token -> Polyomino
+createPolyomino xs token =
+  let points = unzip xs;
+      width  = (maximum $ fst points) + 1;
+      height = (maximum $ snd points) + 1 in
+  Polyomino xs width height token
+
+flipxy :: Polyomino -> Polyomino
+flipxy p@(Polyomino{ parts=(ps), width=w, height=h }) =
+  p{ parts=sort $ flip' ps, width=h, height=w }
+
+  where flip' :: [Location] -> [Location]
+        flip' ((a,b):xs) = (b,a):flip' xs
+        flip' [] = []
+
+flipv :: Polyomino -> Polyomino
+flipv p@(Polyomino{ parts=ps, width=w }) =
+  p{ parts = sort $ map (flip flipv' w) ps  }
+
+  where flipv' :: Location -> Width -> Location
+        flipv' (x,y) w = ((w - 1 - x),y)
+                  
+instance Show Polyomino where
+  show (Polyomino{parts=ps, token=t, width=w, height=h}) =
+
+    intercalate "\n" . splitEvery w $ setTokens [] t (sort ps) w
+
+    where
+      setTokens :: [Token] -> Token -> [Location] -> Int -> [Token]
+      setTokens vs a ((x,y):xs) w = do
+        let vs' = setAt (x + y * w) a ' ' vs
+        setTokens vs' a xs w
+      setTokens vs _ [] _ = vs
+
+      splitEvery _ [] = []
+      splitEvery n list =
+        let (first,rest) = splitAt n list in
+          first : (splitEvery n rest)
+
+      setAt :: Int -> Token -> Token -> [Token] ->[Token]
+      setAt 0 a _ (_:xs) = a : xs
+      setAt 0 a b [] = [a]
+      setAt i a b (x:xs) | i > 0 = x : setAt (i-1) a b xs
+                         | otherwise = []
+      setAt i a b [] | i > 0 = b : setAt (i-1) a b []
+                     | otherwise = []
+
+                     
+
+
+
+
+
+
+          
---
+++ Types.hs
@@ -0,0 +1,22 @@
+module Types (Width, Height, Rectangle(..), Location, Token, row, col, locations)
+where
+
+type Width = Int
+type Height = Int
+
+data Rectangle = Rectangle Int Int Width Height
+  deriving Show
+
+type Location = (Int, Int)
+
+type Token = Char
+
+row :: Location -> Int
+row x = fst x
+
+col :: Location -> Int
+col x = snd x
+
+locations :: Rectangle -> [Location]
+locations (Rectangle x y width height)
+  = [(x + i, y + j) | j <- [0..(height-1)], i <- [0..(width-1)]]
